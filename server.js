7b99805e073b46063af6a81338bc982ec2affd3e
Lukinhaslopessabino <72286265+Lukinhaslopessabino@users.noreply.github.com>
2026-02-19 18:42:34 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("./"));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // 🔐 COLOQUE SEU TOKEN (sem "bot")
 const TELEGRAM_TOKEN = "8575303881:AAG8eV7o6lZIRghNYWBF4BUQ4QiNwq8lKgw";
